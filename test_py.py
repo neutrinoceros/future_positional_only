f58b1d4563d32bdb25e14174dd691e62556d28b9
Clément Robert <cr52@protonmail.com>
2024-06-13 15:37:49 +0200

    fmt

--- test_py.py
+++ test_py.py
@@ -1,14 +1,15 @@
-    
-from future_positional_only import fpo    
+from future_positional_only import fpo
 import pytest
 
 
 def noop(a, b, c, d):
     return
 
+
 depr1 = fpo(kw=["a"])(noop)
 depr2 = fpo(kw=["a", "b"])(noop)
 depr4 = fpo(kw=["a", "a", "c", "d"])(noop)
+
 
 @pytest.mark.parametrize("func", [depr1, depr2, depr4])
 def test_no_warn(func):
@@ -16,18 +17,55 @@ def test_no_warn(func):
 
 
 @pytest.mark.parametrize(
-        "func, args, kwargs, match",
-        [
-            (depr1, (), {"a": 1, "b": 2, "c": 3, "d": 4}, r"Passing 'a' as keyword \(at position 0\) is deprecated"),
-            (depr2, (1,), {"b": 2, "c": 3, "d": 4}, r"Passing 'b' as keyword \(at position 1\) is deprecated"),
-            (depr2, (), {"a": 1, "b": 2, "c": 3, "d": 4}, r"Passing \['a', 'b'\] arguments as keywords \(at positions \[0, 1\] respectively\) is deprecated"),
-            (depr4, (), {"a": 1, "b": 2, "c": 3, "d": 4}, r"Passing \['a', 'b', 'c', 'd'\] arguments as keywords \(at positions \[0, 1, 2, 3\] respectively\) is deprecated"),
-            (depr4, (1,), {"b": 2, "c": 3, "d": 4}, r"Passing \['b', 'c', 'd'\] arguments as keywords \(at positions \[1, 2, 3\] respectively\) is deprecated"),
-            (depr4, (1, 2), {"c": 3, "d": 4}, r"Passing \['c', 'd'\] arguments as keywords \(at positions \[0, 1\] respectively\) is deprecated"),
-            (depr4, (1, 2, 3), {"d": 4}, r"Passing 'd' as keyword \(at position 3\) is deprecated"),
-        ]
+    "func, args, kwargs, match",
+    [
+        (
+            depr1,
+            (),
+            {"a": 1, "b": 2, "c": 3, "d": 4},
+            r"Passing 'a' as keyword \(at position 0\) is deprecated",
+        ),
+        (
+            depr2,
+            (1,),
+            {"b": 2, "c": 3, "d": 4},
+            r"Passing 'b' as keyword \(at position 1\) is deprecated",
+        ),
+        (
+            depr2,
+            (),
+            {"a": 1, "b": 2, "c": 3, "d": 4},
+            r"Passing \['a', 'b'\] arguments as keywords "
+            r"\(at positions \[0, 1\] respectively\) is deprecated",
+        ),
+        (
+            depr4,
+            (),
+            {"a": 1, "b": 2, "c": 3, "d": 4},
+            r"Passing \['a', 'b', 'c', 'd'\] arguments as keywords "
+            r"\(at positions \[0, 1, 2, 3\] respectively\) is deprecated",
+        ),
+        (
+            depr4,
+            (1,),
+            {"b": 2, "c": 3, "d": 4},
+            r"Passing \['b', 'c', 'd'\] arguments as keywords "
+            r"\(at positions \[1, 2, 3\] respectively\) is deprecated",
+        ),
+        (
+            depr4,
+            (1, 2),
+            {"c": 3, "d": 4},
+            r"Passing \['c', 'd'\] arguments as keywords \(at positions \[0, 1\] respectively\) is deprecated",
+        ),
+        (
+            depr4,
+            (1, 2, 3),
+            {"d": 4},
+            r"Passing 'd' as keyword \(at position 3\) is deprecated",
+        ),
+    ],
 )
 def test_warn(func, args, kwargs, match):
     with pytest.warns(FutureWarning, match=match):
         func(*args, **kwargs)
-

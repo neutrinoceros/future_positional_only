c4bc41059f889c69086dd5c469d7e3e1272022dd
Clément Robert <cr52@protonmail.com>
2024-06-13 16:27:47 +0200

    fmt and cleanup

--- _future_positional_only.py
+++ _future_positional_only.py
@@ -1,8 +1,7 @@
-'''Implementation of the positional_defaults package.'''
-
 from typing import Any, Callable, Tuple
 import warnings
 from inspect import Signature
+
 
 def wrap(
     wrapped: Callable[..., Any],
@@ -10,29 +9,31 @@ def wrap(
     signature: Signature,
 ) -> Callable[..., Any]:
     def wrapper(*args, **kwargs):
-        if (deprecated_kwargs := set(kwargs).intersection(names)):
+        if deprecated_kwargs := set(kwargs).intersection(names):
             params = list(signature.parameters)
-            pos2kw:list[tuple[int,str]] = sorted((params.index(name), name) for name in deprecated_kwargs)
+            pos2kw: list[tuple[int, str]] = sorted(
+                (params.index(name), name) for name in deprecated_kwargs
+            )
 
             deprecated_kw = [kw for _, kw in pos2kw]
             if len(deprecated_kw) > 1:
-                names_str=str(deprecated_kw)
-                s="s"
-                arguments=" arguments"
-                respectively=", respectively"
-                pos=[pos for pos, _ in pos2kw]
-                pronoun="them"
+                names_str = str(deprecated_kw)
+                s = "s"
+                arguments = " arguments"
+                respectively = ", respectively"
+                pos = [pos for pos, _ in pos2kw]
+                pronoun = "them"
             else:
-                names_str=repr(deprecated_kw[0])
-                s=arguments=respectively=""
+                names_str = repr(deprecated_kw[0])
+                s = arguments = respectively = ""
                 pos, _ = pos2kw[0]
-                pronoun="it"
+                pronoun = "it"
 
             msg = (
                 f"Passing {names_str}{arguments} as keyword{s} (at position{s} {pos}{respectively}) "
                 "is deprecated and will stop working in a future release. "
                 f"Pass {pronoun} positionally to supress this warning."
-            ) 
+            )
             warnings.warn(msg, FutureWarning, stacklevel=2)
         return wrapped(*args, **kwargs)
 

89d1a6fbd76659bcc5cd5ca6d4ad8c82c4119a3d
Clément Robert <cr52@protonmail.com>
2024-06-19 16:25:44 +0200

    add requirements and plot notebook

--- test_performance.py
+++ test_performance.py
@@ -4,7 +4,8 @@ from functools import wraps
 from timeit import repeat
 
 from future_positional_only import fpo
-
+from positional_defaults import defaults
+from more_itertools import mark_ends
 
 def decorated(func):
     '''Comparison: only call function with args & kwargs.'''
@@ -41,11 +42,17 @@ def func5(a1=None, a2=None, a3=None, a4=None, a5=None, /):
 func0_decorated = decorated(func0)
 func0_decorated.__name__ = 'func0_decorated'
 
+func0_def = defaults(func0)
+func0_def.__name__ = 'func0_def'
+
 func0_fpo = fpo(func0)
 func0_fpo.__name__ = 'func0_fpo'
 
 func1_decorated = decorated(func1)
 func1_decorated.__name__ = 'func1_decorated'
+
+func1_def = defaults(func1)
+func1_def.__name__ = 'func1_def'
 
 func1_fpo = fpo(func1)
 func1_fpo.__name__ = 'func1_fpo'
@@ -53,14 +60,23 @@ func1_fpo.__name__ = 'func1_fpo'
 func5_decorated = decorated(func5)
 func5_decorated.__name__ = 'func5_decorated'
 
+func5_def = defaults(func5)
+func5_def.__name__ = 'func5_def'
+
 func5_fpo = fpo(func5)
 func5_fpo.__name__ = 'func5_fpo'
 
-results = test_performance(
-    func0, func0_decorated, func0_fpo,
-    func1, func1_decorated, func1_fpo,
-    func5, func5_decorated, func5_fpo,
-)
 
-for name, times in results:
-    print(f'{name:19}', sum(times)/len(times))
+results = {
+    "func0": test_performance(func0, func0_decorated, func0_def, func0_fpo),
+    "func1": test_performance(func1, func1_decorated, func1_def, func1_fpo),
+    "func5": test_performance(func5, func5_decorated, func5_def, func5_fpo),
+}
+print("{")
+for _, is_last, (label, res) in mark_ends(results.items()):
+    print(f'"{label}": {{')
+    for _, is_end, (name, times) in mark_ends(res):
+        print(f'  "{name}": {sum(times)/len(times)}', end="\n" if is_end else ",\n")
+    print('  }', end="\n" if is_last else ",\n")
+print("}")
+    

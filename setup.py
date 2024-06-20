465509871852bd6b1578a4810093e1ce01442726
Clément Robert <cr52@protonmail.com>
2024-06-20 11:24:17 +0200

    BLD: treat compilation warnings as errors and fix existing ones

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ ext_modules = []
 if 'FPO_PY_IMPL' not in os.environ:
     py_modules.remove('_future_positional_only')
     ext_modules.append(Extension('_future_positional_only',
-                                 ['_future_positional_only.c']))
+                                 ['_future_positional_only.c'],
+                                 extra_compile_args=["-Werror"]), #, "-Wall", "-Wextra"]),
+                       )
 
 setup(py_modules=py_modules,
       ext_modules=ext_modules)
